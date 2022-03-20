d7479d77c1e0fac55099f84f7c2f7d43fa71bf88
Szymon Koniecko <80451@student.pb.edu.pl>
2022-03-20 13:56:58 +0100

    Wstęp

---
+++ Models/FizzBuzz.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Zad2.Models
+{
+    public class FizzBuzz
+    {
+        [Display(Name = "Twój szczęśliwy numerek")]
+        [Range(1, 1000, ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}."), Required (ErrorMessage = "Pole jest obowiązkowe")]
+        public int? Number { get; set; }
+    }
+}
